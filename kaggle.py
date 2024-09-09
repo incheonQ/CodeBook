2e4552c9b002c528a5c19e82aad2f8b92aee36e7
minkyu kim <logisdatascience@gmail.com>
2024-09-09 11:05:58 +0900

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -1,5 +1,18 @@
 
 def download_dataset(name-of-competition):
+  """
+    Downloads a dataset from Kaggle using Google Colab.
+
+    Parameters:
+    name_of_competition (str): The name of the Kaggle competition or dataset.
+
+    Steps:
+    1. Prompts the user to check if they have the kaggle.json file.
+    2. Installs the Kaggle API client if not already installed.
+    3. Uploads the kaggle.json file using Colab's file upload interface.
+    4. Configures the Kaggle API client with the uploaded kaggle.json file.
+    5. Downloads the dataset associated with the specified competition.
+  """
 
   ans = 'Do you download kaggle.json file on your machine?(y/n)'
 
